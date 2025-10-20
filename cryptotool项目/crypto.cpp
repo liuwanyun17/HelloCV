59dfe1e5a882b140f20d4910fba2dbc356e12ade
liuwanyun17 <18756538095@163.com>
2025-10-20 22:13:35 +0800

    Update crypto.cpp

--- cryptotool项目/crypto.cpp
+++ cryptotool项目/crypto.cpp
@@ -7,11 +7,10 @@ using namespace std;
 char Crypto::enchar(char a,int key){
     int shift1=0;
     char shift2=' ';
-    if (a >= 'A' && a <= 'Z') {             
-             shift1 =((a -'A')+ key) % 26;  
+    if (a >= 'A' && a <='Z') {            
+             shift1 =((a -'A')+ key) % 26; 
              shift2 =shift1+'A' ;
-             return shift2;
-            
+             return shift2;           
 }
     else if (a>='a'&&a<='z'){
         shift1=((a-'a')+key)%26;
@@ -25,12 +24,18 @@ char Crypto::dechar(char a,int key){
     int shift1;
     char shift2;
     if (a >= 'A' && a <= 'Z') {             
-             shift1 =((a -'A')-key) % 26;  
+             shift1 =((a -'A')-key) % 26; 
+             if(shift1<=0) {
+                shift1+=26;
+             }
              shift2 =shift1+'A' ;
             return shift2;
 }
     else if (a>='a'&&a<='z'){
         shift1=((a-'a')-key)%26;
+        if(shift1<=0) {
+                shift1+=26;
+             }
         shift2 =shift1+'a';
         return shift2;
     }
@@ -38,7 +43,7 @@ char Crypto::dechar(char a,int key){
     
 }
 
-string Crypto::encrypto (string &txt,int key){
+string Crypto::encrypto (const string &txt,int key){
     string result;
     for(char a:txt)
     {
@@ -47,7 +52,7 @@ string Crypto::encrypto (string &txt,int key){
     }
     return result;
 }
-string Crypto::decrypto (string &txt,int key){
+string Crypto::decrypto (const string &txt,int key){
     string result;
     for(char a:txt){
         result+=dechar(a,key);
