c10e562120c6f77c559e4f09c02ef5d8acb98e67
liuwanyun <3486299913@qq.com>
2025-10-26 11:40:22 +0800

    Initial commit

---
+++ build/CMakeFiles/trafficlight.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/trafficlight.dir/src/main.cpp.o"
+  "CMakeFiles/trafficlight.dir/src/main.cpp.o.d"
+  "trafficlight"
+  "trafficlight.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/trafficlight.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/trafficlight.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for trafficlight.
+# This may be replaced when dependencies are built.
